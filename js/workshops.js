a01ab022efbae2183d015cf3a3fbeacb7897a824
edvinbasil <edvinbasil@gmail.com>
2018-12-22 19:38:07 +0530

    Added Workshops
    - Details not written
    - Counter needs testing and integration with townscript
    - Lectures will be the same format

---
+++ js/workshops.js
@@ -0,0 +1,117 @@
+/* JS Document */
+
+
+
+$(document).ready(function()
+{
+	"use strict";
+
+
+
+	var header = $('.header');
+	var ctrl = new ScrollMagic.Controller();
+
+	initMenu();
+
+	initMilestones();
+
+	setHeader();
+
+	$(window).on('resize', function()
+	{
+		setHeader();
+	});
+
+	$(document).on('scroll', function()
+	{
+		setHeader();
+	});
+
+
+
+	function setHeader()
+	{
+		if($(window).scrollTop() > 91)
+		{
+			header.addClass('scrolled');
+		}
+		else
+		{
+			header.removeClass('scrolled');
+		}
+	}
+
+
+	function initMenu()
+	{
+		if($('.menu').length)
+		{
+			var hamb = $('.hamburger');
+			var menu = $('.menu');
+			var menuOverlay = $('.menu_overlay');
+
+			hamb.on('click', function()
+			{
+				menu.addClass('active');
+			});
+
+			menuOverlay.on('click', function()
+			{
+				menu.removeClass('active');
+			});
+		}
+	}
+
+
+/* Testing ticket counter...need to add php ticket fetching */
+
+	function initMilestones()
+	{
+		if($('.milestone_counter').length)
+		{
+			var milestoneItems = $('.milestone_counter');
+
+	    	milestoneItems.each(function(i)
+	    	{
+	    		var ele = $(this);
+	    		var endValue = ele.data('end-value');
+	    		var eleValue = ele.text();
+
+	    		var signBefore = "";
+	    		var signAfter = "";
+
+	    		if(ele.attr('data-sign-before'))
+	    		{
+	    			signBefore = ele.attr('data-sign-before');
+	    		}
+
+	    		if(ele.attr('data-sign-after'))
+	    		{
+	    			signAfter = ele.attr('data-sign-after');
+	    		}
+
+	    		var milestoneScene = new ScrollMagic.Scene({
+		    		triggerElement: this,
+		    		triggerHook: 'onEnter',
+		    		reverse:false
+		    	})
+		    	.on('start', function()
+		    	{
+		    		var counter = {value:eleValue};
+		    		var counterTween = TweenMax.to(counter, 4,
+		    		{
+		    			value: endValue,
+		    			roundProps:"value", 
+						ease: Circ.easeOut, 
+						onUpdate:function()
+						{
+							document.getElementsByClassName('milestone_counter')[i].innerHTML = signBefore + counter.value + signAfter;
+						}
+		    		});
+		    	})
+			    .addTo(ctrl);
+	    	});
+		}
+	}
+
+});
