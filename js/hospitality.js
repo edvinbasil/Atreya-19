5dddc3b740558ad8942204d40fa5671e5b3e4f4b
edvinbasil <edvinbasil@gmail.com>
2019-01-08 01:42:26 +0530

    Prerelease 01...almost done
    - Images set
    - Content set
    - Contact details set
    - Hospitality form set
    
    - Ticket links to be added
    - Map to be added
    - Contact form mailto to be added
    - Dates in index page not set

---
+++ js/hospitality.js
@@ -0,0 +1,57 @@
+
+$(document).ready(function()
+{
+	"use strict";
+
+
+	var header = $('.header');
+
+	initMenu();
+
+	setHeader();
+
+	$(window).on('resize', function()
+	{
+		setHeader();
+	});
+
+	$(document).on('scroll', function()
+	{
+		setHeader();
+	});
+
+
+
+	function setHeader()
+	{
+		if($(window).scrollTop() > 91)
+		{
+			header.addClass('scrolled');
+		}
+		else
+		{
+			header.removeClass('scrolled');
+		}
+	}
+
+	function initMenu()
+	{
+		if($('.menu').length)
+		{
+			var hamb = $('.hamburger');
+			var menu = $('.menu');
+			var menuOverlay = $('.menu_overlay');
+
+			hamb.on('click', function()
+			{
+				menu.addClass('active');
+			});
+
+			menuOverlay.on('click', function()
+			{
+				menu.removeClass('active');
+			});
+		}
+	}
+
+});
